a2ad58f2c09e5becfa03f96ed557f0bc8fc31819
Ênio Henrique <enio.henrique12@gmail.com>
2023-03-30 17:49:27 -0300

    Updates

--- assets/index-782b4870.js
+++ assets/index-782b4870.js
@@ -348,4 +348,4 @@ Error generating stack: `+o.message+`
     }
 
 
-`,s2={white:"#FFF","gray-100":"#E1E1E6","gray-300":"#C4C4CC","gray-400":"#8D8D99","gray-500":"#7C7C8A","gray-600":"#323238","gray-700":"#29292E","gray-800":"#202024","gray-900":"#121214","green-300":"#00B37E","green-500":"#00875F","green-700":"#015F43","red-500":"#AB222E","red-700":"#7A1921","yellow-500":"#FBA94C"};function l2(){return Xe(Jy,{theme:s2,children:[" ",q(r1,{basename:"./",children:q(o2,{})}),q(a2,{})]})}il.createRoot(document.getElementById("root")).render(q(E.StrictMode,{children:q(l2,{})}));
+`,s2={white:"#FFF","gray-100":"#E1E1E6","gray-300":"#C4C4CC","gray-400":"#8D8D99","gray-500":"#7C7C8A","gray-600":"#323238","gray-700":"#29292E","gray-800":"#202024","gray-900":"#121214","green-300":"#00B37E","green-500":"#00875F","green-700":"#015F43","red-500":"#AB222E","red-700":"#7A1921","yellow-500":"#FBA94C"};function l2(){return Xe(Jy,{theme:s2,children:[" ",q(r1,{basename:"/pomodoro-timer",children:q(o2,{})}),q(a2,{})]})}il.createRoot(document.getElementById("root")).render(q(E.StrictMode,{children:q(l2,{})}));
